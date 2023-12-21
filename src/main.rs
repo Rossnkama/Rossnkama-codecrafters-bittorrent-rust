56bb7ecb8868a33bd5142ff41c554b7e0e028bf0
Ross Nkama <ssor.nkama@gmail.com>
2023-12-21 22:07:46 +0200

    Attempt 2: Refactor

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,10 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     let first_char = encoded_value.chars().next().unwrap();
     
     // If encoded_value starts with a digit, it's we're dealing with a byte string
-    if first_char.is_digit(10) {
-        decode_bencoded_string(encoded_value)
-    } else if first_char.eq(&'i') {
-        decode_bencoded_integer(encoded_value)
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    match first_char {
+        ch if ch.is_digit(10) => decode_bencoded_string(encoded_value),
+        'i' => decode_bencoded_integer(encoded_value),
+        _ => panic!("Unhandled encoded value: {}", encoded_value),
     }
 }
 
@@ -46,4 +44,3 @@ fn main() {
         println!("unknown command: {}", args[1])
     }
 }
-
