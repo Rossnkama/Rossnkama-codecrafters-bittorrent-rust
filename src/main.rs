21c0a86569a640e413b2cc4e9fea1e63ee2ab1c7
Ross Nkama <ssor.nkama@gmail.com>
2023-12-31 14:29:45 +0200

    Moved to using serde instead of custom parser

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,19 @@
-use serde_json;
+use serde::{Deserialize, Serialize};
 use std::env;
 
-fn decode_bencoded_string(encoded_value: &str) -> serde_json::Value {
-    let colon_index = encoded_value.find(':').unwrap();
-    let number_string = &encoded_value[..colon_index];
-    let number = number_string.parse::<i64>().unwrap();
-
-    let start_index = colon_index + 1;
-    let end_index = start_index + number as usize;
-
-    let string = &encoded_value[start_index..end_index];
-
-    return serde_json::Value::String(string.to_string());
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+struct Torrent {
+    announce: String,
+    info: Info,
 }
 
-fn decode_bencoded_integer(encoded_value: &str) -> serde_json::Value {
-    let integer: String = encoded_value[1..]
-        .chars()
-        .into_iter()
-        .take_while(|ch| ch != &'e')
-        .collect();
-    integer.parse().unwrap()
-}
-
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    let first_char = encoded_value.chars().next().unwrap();
-
-    // If encoded_value starts with a digit, it's we're dealing with a byte string
-    if first_char.is_digit(10) {
-        decode_bencoded_string(encoded_value)
-    } else if first_char.eq(&'i') {
-        decode_bencoded_integer(encoded_value)
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+struct Info {
+    /// Size of a file in bytes for a single-file record
+    length: i64,
+    name: String,
+    piece_length: usize,
+    pieces: serde_bytes::ByteBuf,
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
@@ -44,8 +23,8 @@ fn main() {
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        let decoded_value: Torrent = serde_bencode::from_str(&encoded_value).unwrap();
+        println!("{:#?}", decoded_value);
     } else {
         println!("unknown command: {}", args[1])
     }
