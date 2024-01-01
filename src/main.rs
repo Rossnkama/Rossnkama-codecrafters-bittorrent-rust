aefdde326989080f62588152f37ab91174b5f79c
Ross Nkama <ssor.nkama@gmail.com>
2024-01-02 00:12:21 +0200

    Using serde_bencode instead of custom parser

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
-use serde::{Deserialize, Serialize};
+use serde_bencode::value::Value as SerdeBencodeValue;
 use std::env;
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
-struct Torrent {
-    announce: String,
-    #[serde(rename = "created by")]
-    created_by: String,
-    info: Info,
+fn decode_value(encoded_value: &str) -> SerdeBencodeValue {
+    let serde_data: SerdeBencodeValue = serde_bencode::from_str(encoded_value).unwrap();
+    serde_data
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
-struct Info {
-    /// Size of a file in bytes for a single-file record
-    length: i64,
-    name: String,
-    piece_length: usize,
-    pieces: Vec<u8>,
+fn render_value(decoded_value: SerdeBencodeValue) {
+    match decoded_value {
+        SerdeBencodeValue::Bytes(b) => {
+            println!("{:?}", String::from_utf8_lossy(&b));
+        }
+        SerdeBencodeValue::Int(i) => {
+            println!("{:?}", i);
+        }
+        SerdeBencodeValue::List(l) => {
+            println!("{:?}", l);
+        }
+        SerdeBencodeValue::Dict(d) => {
+            println!("{:?}", d);
+        }
+    }
 }
 
+// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded_value: Torrent = serde_bencode::from_bytes::<Torrent>(&encoded_value.as_bytes()).unwrap();
-        println!("{:#?}", decoded_value);
+        let decoded_value = decode_value(encoded_value);
+        render_value(decoded_value);
     } else {
         println!("unknown command: {}", args[1])
     }
