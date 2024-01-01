81b735e594bc7ab5af4874dafafa57b4acff4545
Ross Nkama <ssor.nkama@gmail.com>
2024-01-02 00:44:52 +0200

    Added list and dict support

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,44 @@
 use serde_bencode::value::Value as SerdeBencodeValue;
-use std::env;
+use std::{env, fmt};
 
 fn decode_value(encoded_value: &str) -> SerdeBencodeValue {
     let serde_data: SerdeBencodeValue = serde_bencode::from_str(encoded_value).unwrap();
     serde_data
 }
 
-fn render_value(decoded_value: SerdeBencodeValue) {
+fn render_value(decoded_value: &SerdeBencodeValue) -> String {
     match decoded_value {
-        SerdeBencodeValue::Bytes(b) => {
-            println!("{:?}", String::from_utf8_lossy(&b));
-        }
-        SerdeBencodeValue::Int(i) => {
-            println!("{:?}", i);
-        }
+        SerdeBencodeValue::Bytes(b) => format!("{}", String::from_utf8_lossy(b)),
+        SerdeBencodeValue::Int(i) => format!("{}", i),
         SerdeBencodeValue::List(l) => {
-            println!("{:?}", l);
+            let list_items: Vec<String> = l.iter().map(|item| render_value(item)).collect();
+            format!("[{}]", list_items.join(", "))
         }
         SerdeBencodeValue::Dict(d) => {
-            println!("{:?}", d);
+            let mut entries = vec![];
+            for (key, value) in d {
+                let rendered_key = String::from_utf8_lossy(key);
+                let rendered_value = render_value(value);
+                entries.push(format!("{}: {}", rendered_key, rendered_value));
+            }
+            format!("{{{}}}", entries.join(", "))
         }
     }
 }
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let command = &args[1];
+    if args.len() > 2 {
+        let command = &args[1];
 
-    if command == "decode" {
-        let encoded_value = &args[2];
-        let decoded_value = decode_value(encoded_value);
-        render_value(decoded_value);
+        if command == "decode" {
+            let encoded_value = &args[2];
+            let decoded_value = decode_value(encoded_value);
+            println!("{}", render_value(&decoded_value));
+        } else {
+            println!("unknown command: {}", args[1])
+        }
     } else {
-        println!("unknown command: {}", args[1])
+        println!("Usage: your_bittorrent.sh decode \"<encoded_value>\"");
     }
 }
