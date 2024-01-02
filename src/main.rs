aaca405030caf7b54fae09cdbbaddc8941f60d99
Ross Nkama <ssor.nkama@gmail.com>
2024-01-02 14:35:03 +0200

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -48,30 +48,38 @@ fn render_value(decoded_value: &SerdeBencodeValue) -> String {
     }
 }
 
+fn calculate_hash(info: &[u8]) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update(info);
+    let result = hasher.finalize();
+    hex::encode(result)
+}
+
+fn handle_decode(encoded_value: &str) {
+    let decoded_value = decode_value(encoded_value);
+    println!("{}", render_value(&decoded_value));
+}
+
+fn handle_info(file_path: &str) {
+    let file = fs::read(file_path).unwrap();
+    let decoded_value: Torrent = serde_bencode::from_bytes(&file).unwrap();
+    let info = serde_bencode::to_bytes(&decoded_value.info).unwrap();
+    let hex_encoded_data = calculate_hash(&info);
+    println!(
+        "Tracker URL: {} Length: {} Info Hash: {}",
+        decoded_value.announce, decoded_value.info.length, hex_encoded_data
+    );
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 2 {
         let command = &args[1];
 
-        if command == "decode" {
-            let encoded_value = &args[2];
-            let decoded_value = decode_value(encoded_value);
-            println!("{}", render_value(&decoded_value));
-        } else if command == "info" {
-            let file_path = &args[2];
-            let file = fs::read(file_path).unwrap();
-            let decoded_value: Torrent = serde_bencode::from_bytes(&file).unwrap();
-            let info = serde_bencode::to_bytes(&decoded_value.info).unwrap();
-            let mut hasher = Sha1::new();
-            hasher.update(info);
-            let result = hasher.finalize();
-            let hex_encoded_data = hex::encode(result);
-            println!(
-                "Tracker URL: {} Length: {} Info Hash: {}",
-                decoded_value.announce, decoded_value.info.length, hex_encoded_data
-            );
-        } else {
-            println!("unknown command: {}", args[1])
+        match command.as_str() {
+            "decode" => handle_decode(&args[2]),
+            "info" => handle_info(&args[2]),
+            _ => println!("unknown command: {}", args[1]),
         }
     } else {
         println!("Usage: your_bittorrent.sh decode \"<encoded_value>\"");
