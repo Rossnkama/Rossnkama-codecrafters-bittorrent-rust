efc51db7213671b839fab74ac0209c710f4e50eb
Ross Nkama <ssor.nkama@gmail.com>
2024-01-05 13:12:15 +0200

    Request tuple types are homogenous

--- src/tracker.rs
+++ src/tracker.rs
@@ -11,16 +11,18 @@ pub fn discover(torrent: &Torrent) -> Result<(), reqwest::Error> {
     // TODO: Handle this error gracefully
     let res: Response = client
         .post(&torrent.announce)
-        .query(&[(
-            "info_hash",
-            &calculate_hash(&info.expect("Info did not unwrap!")),
-            ("peer_id", "00112233445566778899"),
-            ("port", "6881"),
-            ("uploaded", "0"),
-            ("downloaded", "0"),
-            ("left", &torrent.info.length.to_string()),
-            ("compact", "1"),
-        )])
+        .query(&[
+            (
+                "info_hash",
+                calculate_hash(&info.expect("Info did not unwrap!")),
+            ),
+            ("peer_id", "00112233445566778899".to_owned()),
+            ("port", "6881".to_owned()),
+            ("uploaded", "0".to_owned()),
+            ("downloaded", "0".to_owned()),
+            ("left", torrent.info.length.to_string()),
+            ("compact", "1".to_owned()),
+        ])
         .send()?;
 
     // let decoded_value = bencode::decode_value(&body);
