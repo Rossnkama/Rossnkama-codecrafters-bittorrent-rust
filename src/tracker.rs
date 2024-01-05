ddc5f71ab2333b8643f994d639d60efadff12f5d
Ross Nkama <ssor.nkama@gmail.com>
2024-01-05 12:59:59 +0200

    Craft request body

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,13 +1,30 @@
-use reqwest::blocking;
+use reqwest::blocking::{self, Response};
 
 use crate::bencode;
+use crate::hash::calculate_hash;
 use crate::torrent::Torrent;
 
 pub fn discover(torrent: &Torrent) -> Result<(), reqwest::Error> {
-    let body = blocking::get(&torrent.announce)?.text()?;
-    let decoded_value = bencode::decode_value(&body);
-    let val = bencode::render_value(&decoded_value);
+    let client = reqwest::blocking::Client::new();
+    let info = serde_bencode::to_bytes(&torrent.info);
 
-    println!("body = {}", val);
+    // TODO: Handle this error gracefully
+    let res: Response = client
+        .post(&torrent.announce)
+        .query(&[(
+            "info_hash",
+            calculate_hash(&info.unwrap()),
+            ("peer_id", "00112233445566778899"),
+            ("port", "6881"),
+            ("uploaded", "0"),
+            ("downloaded", "0"),
+            ("left", torrent.info.length.to_string()),
+            ("compact", "1"),
+        )])
+        .send()?;
+
+    // let decoded_value = bencode::decode_value(&body);
+    // let val = bencode::render_value(&decoded_value);
+    println!("body = {:?}", res.text());
     Ok(())
 }
