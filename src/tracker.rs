d2e0b71ea9a18ac2dbec420e58bbc695be47563a
Ross Nkama <ssor.nkama@gmail.com>
2024-01-05 22:12:39 +0200

    Custom serializer for urlencoding

--- src/tracker.rs
+++ src/tracker.rs
@@ -10,7 +10,7 @@ where
     let mut encoded = String::with_capacity(3 * t.len());
     for &byte in t {
         encoded.push('%');
-        encoded.push_str(&hex::encode([byte]));
+        encoded.push_str(&hex::encode(&[byte]));
     }
     serializer.serialize_str(&encoded)
 }
