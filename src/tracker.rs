6d6ce6a0f7aa7650367f0518ed592342fa92d5fd
Ross Nkama <ssor.nkama@gmail.com>
2024-01-05 17:07:01 +0200

    Add tracker request struct

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,24 +1,42 @@
 use reqwest::blocking::Response;
-
+use serde::Serialize;
 use crate::hash::calculate_hash;
 use crate::torrent::Torrent;
 
+#[derive(Serialize)]
+pub struct TrackerRequest {
+    info_hash: Vec<u8>,
+    peer_id: String,
+    port: u16,
+    uploaded: usize,
+    downloaded: usize,
+    left: usize,
+    compact: u8,
+}
+
+impl TrackerRequest {
+    pub fn new(torrent: &Torrent) -> Self {
+        let info = serde_bencode::to_bytes(&torrent.info).unwrap();
+        let info_hash = calculate_hash(&info);
+        Self {
+            info_hash,
+            peer_id: "00112233445566778899".to_owned(),
+            port: 6881,
+            uploaded: 0,
+            downloaded: 0,
+            left: torrent.info.length,
+            compact: 1,
+        }
+    }
+}
+
 pub fn discover(torrent: &Torrent) -> Result<(), reqwest::Error> {
     let client = reqwest::blocking::Client::new();
-    let info = serde_bencode::to_bytes(&torrent.info);
+    let request = TrackerRequest::new(torrent);
 
-    // TODO: Handle this error gracefully
     let res: Response = client
         .get(&torrent.announce)
-        .query(&[
-            ("info_hash", hex::encode(calculate_hash(&info.unwrap()))),
-            ("peer_id", "00112233445566778899".to_owned()),
-            ("port", "6881".to_owned()),
-            ("uploaded", "0".to_owned()),
-            ("downloaded", "0".to_owned()),
-            ("left", torrent.info.length.to_string()),
-            ("compact", "1".to_owned()),
-        ])
+        .query(&request)
         .send()?;
 
     println!("body = {:?}", res);
